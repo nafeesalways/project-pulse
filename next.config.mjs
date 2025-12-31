b68af674020f110e2ac2355755e58df96554095a
nafeesalways <nafeezmy@gmail.com>
2025-12-31 18:40:59 +0600

    update next.config for Next.js 16 Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Externalize MongoDB from webpack
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push({
-        'mongodb': 'commonjs mongodb',
-        'mongodb-client-encryption': 'commonjs mongodb-client-encryption',
-      });
-    }
-    return config;
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['mongodb'],
-  },
+  // Use empty turbopack config to silence warning
+  turbopack: {},
 };
 
 export default nextConfig;
